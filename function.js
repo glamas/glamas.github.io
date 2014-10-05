cc080811de7688dd3fceaa8676175d883b81d700
glamas <glamas@163.com>
2014-10-06 00:35:34 +0800

    correct a small fault

--- function.js
+++ function.js
@@ -14,10 +14,10 @@ xmlhttp.onreadystatechange=function()
   {
   if (xmlhttp.readyState==4 && xmlhttp.status==200)
     {
-        txt="<div onclick='display_none(this)' >";
-    x=xmlhttp.responseXML.documentElement.getElementsByTagName("CD");
+        x=xmlhttp.responseXML.documentElement.getElementsByTagName("CD");
     for (i=0;i<x.length;i++)
     {
+        txt="<div onclick='display_none(this)' >";
         txt=txt + "<div class='record'>";
         xx=x[i].getElementsByTagName("TITLE");
         {
