30aaadcada0f8f0b0c51ea66545fb0b44c33a669
glamas <glamas@163.com>
2014-10-06 00:11:49 +0800

    split js&html

---
+++ function.js
@@ -0,0 +1,74 @@
+function loadXMLDoc(url)
+{
+var xmlhttp;
+var txt,x,xx,i;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    txt="<table border='1'><tr><th>Title</th><th>Artist</th></tr>";
+    x=xmlhttp.responseXML.documentElement.getElementsByTagName("CD");
+    for (i=0;i<x.length;i++)
+      {
+      txt=txt + "<tr>";
+      xx=x[i].getElementsByTagName("TITLE");
+        {
+        try
+          {
+          txt=txt + "<td>" + xx[0].firstChild.nodeValue + "</td>";
+          }
+        catch (er)
+          {
+          txt=txt + "<td> </td>";
+          }
+        }
+      xx=x[i].getElementsByTagName("ARTIST");
+        {
+        try
+          {
+          txt=txt + "<td>" + xx[0].firstChild.nodeValue + "</td>";
+          }
+        catch (er)
+          {
+          txt=txt + "<td> </td>";
+          }
+        }
+      txt=txt + "</tr>";
+      }
+    txt=txt + "</table>";
+    document.getElementById('txtCDInfo').innerHTML=txt;
+    }
+  }
+xmlhttp.open("GET",url,true);
+xmlhttp.send();
+}
+
+
+function display_none(name_id)
+{
+    //alert(name_id.childNodes.length);
+    if(name_id.childNodes.length == 5){
+        if(name_id.childNodes[3].style.display=='none'){
+            name_id.childNodes[3].style.display='inline';
+        }
+        else if(name_id.childNodes[3].style.display=='inline'){
+            name_id.childNodes[3].style.display='none';
+        }
+    }
+    if(name_id.childNodes.length == 2){        
+        if(name_id.childNodes[2].style.display=='none'){
+            name_id.childNodes[2].style.display='inline';
+        }
+        else if(name_id.childNodes[3].style.display=='inline'){
+            name_id.childNodes[3].style.display='none';
+        }
+    }
+}
