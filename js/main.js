48b172349a79195add39f4e751cf129b0de93b8e
glamas <glamas@163.com>
2016-07-02 22:36:06 +0800

    clear code

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 window.onload = function () {
   var default_config = {
-    "ignore_cat":["css","js"],
-    "tpl_ver": "0.1.0",
     "username": "glamas",
     "repo": "glamas.github.io",
-    "github_api": "https://api.github.com/repos/"
+    "github_api": "https://api.github.com/repos/",
+    "ignore_dir":["css","js","image"],
+    "markdown_ext":".md"
   }
   var converter = new showdown.Converter();
   // converter.setOption('noHeaderId', true);
@@ -27,7 +27,7 @@ window.onload = function () {
         this.$http.get(this.config.github_api+this.config.username+'/'+this.config.repo+'/contents').then(function(response){
           var tmp=[]
           for (var i = response.data.length - 1; i >= 0; i--) {
-            if (response.data[i].type == "dir" && this.config.ignore_cat.indexOf(response.data[i].name) == -1) {
+            if (response.data[i].type == "dir" && this.config.ignore_dir.indexOf(response.data[i].name) == -1) {
               tmp.push({'category':response.data[i].name})
             }
           }
